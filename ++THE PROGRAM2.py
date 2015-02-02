031f67267907fae525ff501a17df79e83e08b512
Tom Davies <space.man619@googlemail.com>
2015-02-02 15:04:46 +0000

    -Short Sequences
    Removed the code for shortening BLAST sequences to prevent crashes

--- ++THE PROGRAM2.py
+++ ++THE PROGRAM2.py
@@ -7,23 +7,15 @@ for seq_origin in SeqIO.parse(file_gbk, "genbank"):
         print seq_origin.id, ":", seq_origin.description
 print
 
-#Generating smaller sequences for BLAST
-u = 0
-length = len(seq_origin.seq)
-s = length/10
-
-for cycle in range(1, 11):
-	t = cycle*s
-	#Send Sequence to BLASTx
-	print("Running BLAST %d , Please Wait." % cycle)
-	result_handle = NCBIWWW.qblast("blastx", "nr", seq_origin.seq[u:t])
-	save_file = open("origin_blast" + str(cycle) + ".xml","w")
-	save_file.write(result_handle.read())
-	save_file.close()
-	result_handle.close()
-	result_handle = open("origin_blast" + str(cycle) + ".xml")
-	print
-	u = t
+#Send Sequence to BLASTx
+print("Running BLAST , Please Wait.")
+result_handle = NCBIWWW.qblast("blastx", "nr", seq_origin.seq)
+save_file = open("origin_blast.xml","w")
+save_file.write(result_handle.read())
+save_file.close()
+result_handle.close()
+result_handle = open("origin_blast.xml")
+print
 
 print("Press Any Key to Exit...")
 raw_input()
