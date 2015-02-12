22b98cbc809c9a0a93a5c650032f19a463fc9e6d
Tom Davies <thomas.davies-7@student.manchester.ac.uk>
2015-02-12 20:33:44 +0000

    New initial genbank file (danio rerio cd79a) Pull final hit ID from BLAST for taxonomy search

--- overblast.py
+++ overblast.py
@@ -1,4 +1,4 @@
-from Bio import SeqIO
+from Bio import SeqIO, SearchIO
 from Bio.Blast import NCBIWWW
 import os
 
@@ -10,15 +10,19 @@ for seq_origin in SeqIO.parse(file_gbk, "genbank"):
         print seq_origin.id, ":", seq_origin.description
 print
 
-#Send Sequence to BLASTx
+#Send Sequence to BLASTn
 print("Running BLAST , Please Wait.")
-result_handle = NCBIWWW.qblast("blastx", "nr", seq_origin.seq)
+result_handle = NCBIWWW.qblast("blastn", "nr", seq_origin.seq, entrez_query="all[filter] NOT((srcdb_refseq_model[prop] AND biomol_rna[prop]))")
 save_file = open("origin_blast.xml","w")
 save_file.write(result_handle.read())
 save_file.close()
 result_handle.close()
 result_handle = open("origin_blast.xml")
-print
 
-print("Press Any Key to Exit...")
-raw_input()
+blast_table = SearchIO.read('origin_blast.xml', 'blast-xml')
+final_hit = blast_table[-1].id.split("|")
+final_id = final_hit[3]
+print(final_hit)
+
+'''print("Press Any Key to Exit...")
+raw_input()'''
