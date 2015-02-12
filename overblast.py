5f133ce072cea7fd1e97c0a6ea77d496f88259d7
Tom Davies <thomas.davies-7@student.manchester.ac.uk>
2015-02-12 14:11:18 +0000

    Tidying Up - removed dupes

--- overblast.py
+++ overblast.py
@@ -1,22 +1,24 @@
 from Bio import SeqIO
 from Bio.Blast import NCBIWWW
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear') #clear screen
+
 file_gbk = raw_input("genbank file? ")
 # Parse genbank files
 for seq_origin in SeqIO.parse(file_gbk, "genbank"):
-        print(seq_origin.id)
-        print(repr(seq_origin.seq))
-        print(len(seq_origin))
+        print seq_origin.id, ":", seq_origin.description
 print
 
-#Send Sequence to BLASTn
-print("Running BLAST, Please Wait.")
+#Send Sequence to BLASTx
+print("Running BLAST , Please Wait.")
 result_handle = NCBIWWW.qblast("blastx", "nr", seq_origin.seq)
 save_file = open("origin_blast.xml","w")
 save_file.write(result_handle.read())
 save_file.close()
 result_handle.close()
 result_handle = open("origin_blast.xml")
-
 print
+
 print("Press Any Key to Exit...")
 raw_input()
