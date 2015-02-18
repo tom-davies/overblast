ec08bb292093543a5f59470dc8cba97a123e02bb
unknown <mfbx2td4@L-C07SDG321106.ds.man.ac.uk>
2015-02-18 10:32:01 +0000

    Removed trace Taxon length and source added to lists (ready for table) texttable imported (not installed in uni) Commented out initial BLAST to make testing faster Some print functions end with ")," to prevent newlines 	This should be changed for Python3

--- overblast.py
+++ overblast.py
@@ -1,11 +1,11 @@
 from Bio import SeqIO, SearchIO, Entrez
 from Bio.Blast import NCBIWWW
 import os
-import pdb; pdb.set_trace()
+#import texttable as tt
 
 os.system('cls' if os.name == 'nt' else 'clear') #clear screen
 
-file_gbk = raw_input("genbank file? ")
+'''file_gbk = raw_input("genbank file? ")
 # Parse genbank files
 for seq_origin in SeqIO.parse(file_gbk, "genbank"):
     print seq_origin.id, ":", seq_origin.description
@@ -18,21 +18,27 @@ save_file = open("origin_blast.xml","w")
 save_file.write(result_handle.read())
 save_file.close()
 result_handle.close()
-result_handle = open("origin_blast.xml")
+result_handle = open("origin_blast.xml")'''
 
 blast_table = SearchIO.read('origin_blast.xml', 'blast-xml')
+names = []
+tax_length = []
+print("Taxonomy Search in Progress, Please Wait."),
 for x in range(50):
     hit = blast_table[x].id.split("|")
     the_id = hit[1]
-    print(the_id)
     # Entrez Search
     Entrez.email = "thomas.davies-7@student.manchester.ac.uk"
     search = Entrez.efetch(db="nuccore", id=the_id, retmode="xml")
     records = Entrez.read(search)
     taxon = records[0]["GBSeq_taxonomy"].split("; ")
     name = records[0]["GBSeq_source"]
-    print(name)
-    print (len(taxon))
+    names.append(name)
+    tax_length.append(len(taxon))
+    print('.'),
+
+print(names)
+print(tax_length)
 
 print("Press Any Key to Exit...")
 raw_input()
