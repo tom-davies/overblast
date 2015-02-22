9026840bc0a99593ac1b055de72b2cef196d8bf0
Tom Davies <thomas.davies-7@student.manchester.ac.uk>
2015-02-22 22:11:29 +0000

    Trying to get Entrez.read to work on the second iteration

--- entrez_test.py
+++ entrez_test.py
@@ -1,5 +1,6 @@
 from Bio import SeqIO, SearchIO, Entrez
 import os
+import pdb
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -10,19 +11,17 @@ print("Taxonomy Search in Progress, Please Wait."),
 for x in range(50):
     hit = blast_table[x].id.split("|")
     the_id = hit[1]
+    print(the_id)
     # Entrez Search
+    #pdb.set_trace()
     Entrez.email = "thomas.davies-7@student.manchester.ac.uk"
     search = Entrez.efetch(db="nuccore", id=the_id, retmode="xml")
-    print('ding')
     records = Entrez.parse(search)
-    print('dong')
     for record in records:
-        taxon = record["GBSeq_taxonomy"].split("; ")
-        print('the witch')
-        name = record["GBSeq_source"]
-        print('is dead')
+        #print(record["GBSeq_taxonomy"].split("; "))
+        name = record['GBSeq_source']
         names.append(name)
-        tax_length.append(len(taxon))
+        tax_length.append(record["GBSeq_taxonomy"].split("; "))
     print('.'),
 
 print(names)
--- overblast.py
+++ overblast.py
@@ -5,7 +5,7 @@ import os
 
 os.system('cls' if os.name == 'nt' else 'clear') #clear screen
 
-'''file_gbk = raw_input("genbank file? ")
+file_gbk = raw_input("genbank file? ")
 # Parse genbank files
 for seq_origin in SeqIO.parse(file_gbk, "genbank"):
     print seq_origin.id, ":", seq_origin.description
@@ -14,32 +14,29 @@ print
 #Send Sequence to BLASTn
 print("Running BLAST , Please Wait.")
 result_handle = NCBIWWW.qblast("blastn", "nr", seq_origin.seq, entrez_query="all[filter] NOT((srcdb_refseq_model[prop] AND biomol_rna[prop]))")
-save_file = open("origin_blast.xml","w")
-save_file.write(result_handle.read())
-save_file.close()
+blast_file = open("origin_blast.xml","w")
+blast_file.write(result_handle.read())
+blast_file.close()
 result_handle.close()
-result_handle = open("origin_blast.xml")'''
+result_handle = open("origin_blast.xml")
 
 blast_table = SearchIO.read('origin_blast.xml', 'blast-xml')
 names = []
 tax_length = []
-print("Taxonomy Search in Progress, Please Wait."),
+print("Taxonomy Search in Progress, Please Wait.")
 for x in range(50):
     hit = blast_table[x].id.split("|")
     the_id = hit[1]
     # Entrez Search
     Entrez.email = "thomas.davies-7@student.manchester.ac.uk"
     search = Entrez.efetch(db="nuccore", id=the_id, retmode="xml")
-    print('ding')
     records = Entrez.read(search)
-    print('dong')
     taxon = records[0]["GBSeq_taxonomy"].split("; ")
-    print('the witch')
     name = records[0]["GBSeq_source"]
-    print('is dead')
     names.append(name)
     tax_length.append(len(taxon))
-    print('.'),
+    print('.')
+    search.close()
 
 print(names)
 print(tax_length)
