f904c54b6e5ad1b312c66a7aa2e9f6382ef98308
Tom Davies <thomas.davies-7@student.manchester.ac.uk>
2015-03-13 14:35:24 +0000

    Testing mammalian BLAST Starting to work out duplicate prevention 	Some kind of if/then

--- test.py
+++ test.py
@@ -2,17 +2,35 @@ from Bio import SeqIO
 from Bio.Blast import NCBIWWW
 file_gbk = raw_input("genbank file? ")
 # Parse genbank files
-for seq_origin in SeqIO.parse(file_gbk, "genbank"):
+'''for seq_origin in SeqIO.parse(file_gbk, "genbank"):
         print seq_origin.id, ":", seq_origin.description
 print
 
-#Generating smaller sequences for BLAST
-u = 0
-length = len(seq_origin.seq)
-s = length/10
+print("Running Mammalian BLAST , Please Wait.")
+result_handle = NCBIWWW.qblast("blastn", "nr", seq_origin.seq, entrez_query="all[filter] txid40674[ORGN] NOT((srcdb_refseq_model[prop] AND biomol_rna[prop]))")
+blast_file = open("origin_blast.xml","w")
+blast_file.write(result_handle.read())
+blast_file.close()
+result_handle.close()
+result_handle = open("origin_blast.xml")'''
 
-for number in range(1, 11):
-	t = number*s
-	print(seq_origin.seq[u : t])
-	print
-	u = t
+blast_table = SearchIO.read('origin_blast.xml', 'blast-xml')
+print("\aTaxonomy Search in Progress, Please Wait.")
+names = []
+taxons = []
+for x in xrange(50):
+    hit = blast_table[x].id.split("|")
+    the_id = hit[1]
+    print(x+1),
+    # Entrez Search
+    Entrez.email = "thomas.davies-7@student.manchester.ac.uk"
+    search = Entrez.efetch(db="nuccore", id=the_id, rettype="gb", retmode="text")
+    for record in SeqIO.parse(search, "genbank"):
+        name = record.annotations['organism']
+        names.append(name)
+        taxon = record.annotations['taxonomy']
+        taxons.append(len(taxon))
+        print("\t" +name),
+        print("\t\t"),
+        print(len(taxon))
+    search.close()
