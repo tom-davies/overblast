d51d2596b48a5b90c93a11bc1c08b0b76e3fa133
Tom Davies <thomas.davies-7@student.manchester.ac.uk>
2015-03-13 14:33:05 +0000

    More visible output Better commenting out of unused code

--- overblast.py
+++ overblast.py
@@ -1,35 +1,33 @@
 from Bio import SeqIO, SearchIO, Entrez
 from Bio.Blast import NCBIWWW
 import os
-import pdb
-#import texttable as tt
 
 os.system('cls' if os.name == 'nt' else 'clear') #clear screen
 
-file_gbk = raw_input("genbank file? ")
-# Parse genbank files
-for seq_origin in SeqIO.parse(file_gbk, "genbank"):
-    print seq_origin.id, ":", seq_origin.description
-    print seq_origin
-print
+# file_gbk = raw_input("genbank file? ")
+# # Parse genbank files
+# for seq_origin in SeqIO.parse(file_gbk, "genbank"):
+#     print seq_origin.id, ":", seq_origin.description
+#     print seq_origin
+# print
+#
+# #Send Sequence to BLASTn
+# print("Running BLAST , Please Wait.")
+# result_handle = NCBIWWW.qblast("blastn", "nr", seq_origin.seq, entrez_query="all[filter] NOT((srcdb_refseq_model[prop] AND biomol_rna[prop]))")
+# blast_file = open("origin_blast.xml","w")
+# blast_file.write(result_handle.read())
+# blast_file.close()
+# result_handle.close()
+# result_handle = open("origin_blast.xml")
 
-#Send Sequence to BLASTn
-print("Running BLAST , Please Wait.")
-result_handle = NCBIWWW.qblast("blastn", "nr", seq_origin.seq, entrez_query="all[filter] NOT((srcdb_refseq_model[prop] AND biomol_rna[prop]))")
-blast_file = open("origin_blast.xml","w")
-blast_file.write(result_handle.read())
-blast_file.close()
-result_handle.close()
-result_handle = open("origin_blast.xml")
-
-pdb.set_trace()
 blast_table = SearchIO.read('origin_blast.xml', 'blast-xml')
-print("Taxonomy Search in Progress, Please Wait."),
+print("Taxonomy Search in Progress, Please Wait.")
 names = []
 taxons = []
-for x in range(50):
+for x in xrange(50):
     hit = blast_table[x].id.split("|")
     the_id = hit[1]
+    print(x+1),
     # Entrez Search
     Entrez.email = "thomas.davies-7@student.manchester.ac.uk"
     search = Entrez.efetch(db="nuccore", id=the_id, rettype="gb", retmode="text")
@@ -38,10 +36,10 @@ for x in range(50):
         names.append(name)
         taxon = record.annotations['taxonomy']
         taxons.append(len(taxon))
-    print('.'),
+        print("\t" +name),
+        print("\t\t"),
+        print(len(taxon))
     search.close()
 
-print(names)
-print(taxons)
 print("Press Any Key to Exit...")
 raw_input()
