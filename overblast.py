8f69f07b989f5ab557dde4695b0a78c564d2616c
Tom Davies <thomas.davies-7@student.manchester.ac.uk>
2015-04-10 16:14:17 +0100

    Moved BLASTing and Taxonomy searches to functions Makes them easier to use & less diff when excluded for testing

--- overblast.py
+++ overblast.py
@@ -1,45 +1,59 @@
-from Bio import SeqIO, SearchIO, Entrez
+from Bio import SeqIO, Entrez
 from Bio.Blast import NCBIWWW, NCBIXML
+from prettytable import PrettyTable as pt
 import os
 
 os.system('cls' if os.name == 'nt' else 'clear') #clear screen
 
-# file_gbk = raw_input("genbank file? ")
-# # Parse genbank files
-# for seq_origin in SeqIO.parse(file_gbk, "genbank"):
-#     print seq_origin.id, ":", seq_origin.description
-# print
-#
-# #Send Sequence to BLASTn
-# print("Running BLAST , Please Wait.")
-# result_handle = NCBIWWW.qblast("blastn", "nr", seq_origin.seq, entrez_query="all[filter] NOT((srcdb_refseq_model[prop] AND biomol_rna[prop]))")
-# blast_file = open("origin_blast.xml","w")
-# blast_file.write(result_handle.read())
-# blast_file.close()
-# result_handle.close()
-# result_handle = open("origin_blast.xml")
+file_gbk = raw_input("genbank file? ")
+#Parse genbank files
+for seq_origin in SeqIO.parse(file_gbk, "genbank"):
+    print seq_origin.id, ":", seq_origin.description
+print
 
-result_handle = open("origin_blast.xml")
-blast_record = NCBIXML.read(result_handle)
-print("\aTaxonomy Search in Progress, Please Wait.")
-names = []
-taxons = []
-for alignment in blast_record.alignments:
-    hit = alignment.title.split("|")
-    the_id = hit[1]
-    # Entrez Search
-    Entrez.email = "thomas.davies-7@student.manchester.ac.uk"
-    search = Entrez.efetch(db="nuccore", id=the_id, rettype="gb", retmode="text")
-    for record in SeqIO.parse(search, "genbank"):
-        name = record.annotations['organism']
-        names.append(name)
-        taxon = record.annotations['taxonomy']
-        taxons.append(len(taxon))
-        print("\t" +name),
-        print("\t\t"),
-        print(len(taxon))
-    search.close()
-print(list(set(names)))
-print(list(set(taxons)))
+def blast(sequence, run):
+    "Send Sequence to BLASTn"
+    print("Running BLAST , Please Wait.")
+    result_handle = NCBIWWW.qblast("blastn", "nr", sequence, entrez_query="all[filter] NOT((srcdb_refseq_model[prop] AND biomol_rna[prop]))")
+    blast_file = open("blast%d.xml","w" %run)
+    blast_file.write(result_handle.read())
+    blast_file.close()
+    result_handle.close()
+    return;
+
+#blast(sequence = seq_origin.seq,1)
+
+def tax(run):
+    result_handle = open("blast%d.xml" %run)
+    blast_record = NCBIXML.read(result_handle)
+    print("\aTaxonomy Search in Progress, Please Wait.")
+    names = []
+    taxons = []
+    seqs = []
+    for alignment in blast_record.alignments:
+        hit = alignment.title.split("|")
+        the_id = hit[1]
+        # Entrez Search
+        Entrez.email = "thomas.davies-7@student.manchester.ac.uk"
+        search = Entrez.efetch(db="nuccore", id=the_id, rettype="gb", retmode="text")
+        for record in SeqIO.parse(search, "genbank"):
+            name = record.annotations['organism']
+            names.append(name)
+            taxon = record.annotations['taxonomy']
+            taxons.append(len(taxon))
+        search.close()
+        #seqs.append(hsp.sbjct)
+        print(".")
+    return;
+
+tax(1)
+
+table = pt()
+table.add_column("Names", names)
+table.add_column("Taxon Length", taxons)
+table.add_column("HSP", seqs)
+table.align = "l"
+print(table)
+
 print("Press Any Key to Exit...")
 raw_input()
