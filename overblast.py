8a879ae4511471b074e0fe90356eea5eab9f0142
Tom Davies <thomas.davies-7@student.manchester.ac.uk>
2015-04-11 19:35:37 +0100

    Turned tables into a function Handled repeat file names

--- overblast.py
+++ overblast.py
@@ -11,20 +11,18 @@ for seq_origin in SeqIO.parse(file_gbk, "genbank"):
     print seq_origin.id, ":", seq_origin.description
 print
 
-def blast(sequence, run):
+def blast(sequence, run, addn):
     "Send Sequence to BLASTn"
     print("Running BLAST , Please Wait.")
-    result_handle = NCBIWWW.qblast("blastn", "nr", sequence, entrez_query="all[filter] NOT((srcdb_refseq_model[prop] AND biomol_rna[prop]))")
-    blast_file = open("blast%d.xml","w" %run)
+    result_handle = NCBIWWW.qblast("blastn", "nr", sequence, entrez_query="all[filter] NOT((srcdb_refseq_model[prop] AND biomol_rna[prop])) %s" %addn)
+    blast_file = open("blast%s.xml","w" %run)
     blast_file.write(result_handle.read())
     blast_file.close()
     result_handle.close()
     return;
 
-#blast(sequence = seq_origin.seq,1)
-
 def tax(run):
-    result_handle = open("blast%d.xml" %run)
+    result_handle = open("blast%s.xml" %run)
     blast_record = NCBIXML.read(result_handle)
     print("\aTaxonomy Search in Progress, Please Wait.")
     names = []
@@ -46,14 +44,22 @@ def tax(run):
         print(".")
     return;
 
-tax(1)
+def dist_table():
+    table = pt()
+    table.add_column("Names", names)
+    table.add_column("Taxon Length", taxons)
+    table.add_column("HSP", seqs)
+    table.align = "l"
+    print(table)
 
-table = pt()
-table.add_column("Names", names)
-table.add_column("Taxon Length", taxons)
-table.add_column("HSP", seqs)
-table.align = "l"
-print(table)
+#blast(sequence = seq_origin.seq, run = "01", addn = "")
+
+#tax(1)
+
+#dist_table()
+
+#Run Mammalian BLAST
+#blast(sequence = , run = "02", addn = "tx40674[orgn]")
 
 print("Press Any Key to Exit...")
 raw_input()
