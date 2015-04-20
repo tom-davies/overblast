75dbf2a16616a599f6b6629fe853bbbcc4beda2e
Tom Davies <thomas.davies-7@student.manchester.ac.uk>
2015-04-20 14:10:50 +0100

    Fixed ID reading Tidied up terminal formatting

--- overblast.py
+++ overblast.py
@@ -9,14 +9,16 @@ import os
 
 os.system('cls' if os.name == 'nt' else 'clear') #clear screen
 
-file_gbk = raw_input("genbank file? ")
+file_gbk = raw_input("Genbank Filename? ")
+cycles = input("How many cycles before mammalian search? ")
 #Parse genbank files
 for seq_origin in SeqIO.parse(file_gbk, "genbank"):
     print seq_origin.id, ":", seq_origin.description
-print
 
 def blast(sequence, run, addn):
     #Send Sequence to BLASTn
+    print
+    print
     print("Running BLAST cycle #%s , Please Wait." % run)
     result_handle = NCBIWWW.qblast("blastn", "nr", sequence, entrez_query="all[filter] NOT((srcdb_refseq_model[prop] AND biomol_rna[prop])) %s" % addn)
     blast_file = open("blast%s.xml" % run,"w")
@@ -29,7 +31,7 @@ def phylo(run):
     #Draw phylogenic tree from BLAST
     result_handle = open("blast%s.xml" % run)
     blast_record = NCBIXML.read(result_handle)
-    print("Collecting Sequences")
+    print("\tCollecting Sequences")
     def get_seqs(source):
             for aln in source:
                 for hsp in aln.hsps:
@@ -37,35 +39,40 @@ def phylo(run):
                     break
     seqs = get_seqs(blast_record.alignments,)
     SeqIO.write(seqs, 'Phylo/family.fasta', 'fasta')
-    print("Aligning Sequences")
+    print("\tAligning Sequences")
     cmdline = MuscleCommandline(input="Phylo/family.fasta", out="Phylo/family.aln", clw=True)
     cmdline()
     AlignIO.convert("Phylo/family.aln", "clustal", "Phylo/family.phy", "phylip-relaxed")
-    print("Generating Tree...")
+    print("\tGenerating Tree...")
     cmdline = PhymlCommandline(input="Phylo/family.phy")
     out_log, err_log = cmdline()
-    print("Drawing Tree")
+    print("\tDrawing Tree")
     tree = Phylo.read("Phylo/family.phy_phyml_tree.txt", "newick")
-    Phylo.draw_ascii(tree)
+    #Phylo.draw_ascii(tree)
     return;
 
 blast(sequence = seq_origin.seq, run = "01", addn = "")
 phylo(run = "01")
 tree_out = open("Phylo/family.phy_phyml_tree.txt")
 tree_out.seek(1)
-new_id = tree_out.read(8)
-print(new_id)
+new_id = tree_out.read(100).split(":")
+true_id = new_id[0]
+print(true_id)
 
-for x in xrange(2, 10):
-    blast(sequence = new_id, run = "%02d" % x, addn= "")
+for x in xrange(2, cycles):
+    blast(sequence = true_id, run = "%02d" % x, addn= "")
     phylo(run = "%02d" % x)
     tree_out = open("Phylo/family.phy_phyml_tree.txt")
     tree_out.seek(1)
-    new_id = tree_out.read(8)
-    print(new_id)
+    new_id = tree_out.read(100).split(":")
+    true_id = new_id[0]
+    print(true_id)
 
 #Run Mammalian BLAST
-#blast(sequence = , run = "LAST", addn = "tx40674[orgn]")
+blast(sequence = true_id, run = "LAST", addn = "tx40674[orgn]")
+phylo(run = "LAST")
+tree = Phylo.read("Phylo/family.phy_phyml_tree.txt", "newick")
+Phylo.draw_ascii(tree)
 
 print("Press Any Key to Exit...")
 raw_input()
